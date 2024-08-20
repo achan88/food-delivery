733caf98d8e6349901934c9e4700a7a688cd82db
Andrew Chan <andrewhongchan@gmail.com>
2024-08-20 10:57:52 -0400

    Add Navbar search icons

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -13,6 +13,11 @@ const Navbar = () => {
       </ul>
       <div className="navbar-right">
         <img src={assets.search_icon} alt="" />
+        <div className="navbar-search-icon">
+            <img src={assets.basket_icon} alt="" />
+            <div className="dot"></div>
+        </div>
+        <button>sign in</button>
       </div>
     </div>
   );
