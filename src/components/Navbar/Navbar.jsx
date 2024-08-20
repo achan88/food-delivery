b9c475fd2ed7fcfa19c284805d54418757ee27fb
Andrew Chan <andrewhongchan@gmail.com>
2024-08-20 11:09:15 -0400

    feat: Add ability to track state of selected menu item

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,21 +1,24 @@
 import "./Navbar.css";
 import { assets } from "../../assets/assets";
+import { useState } from "react";
 
 const Navbar = () => {
+  9;
+  const [menu, setMenu] = useState("home");
   return (
     <div className="navbar">
-      <img src={assets.logo} alt="" className="logo"/>
+      <img src={assets.logo} alt="" className="logo" />
       <ul className="navbar-menu">
-        <li>Home</li>
-        <li>Menu</li>
-        <li>Mobile-app</li>
-        <li>Contact Us</li>
+        <li>home</li>
+        <li>menu</li>
+        <li>mobile-app</li>
+        <li>contact us</li>
       </ul>
       <div className="navbar-right">
         <img src={assets.search_icon} alt="" />
         <div className="navbar-search-icon">
-            <img src={assets.basket_icon} alt="" />
-            <div className="dot"></div>
+          <img src={assets.basket_icon} alt="" />
+          <div className="dot"></div>
         </div>
         <button>sign in</button>
       </div>
