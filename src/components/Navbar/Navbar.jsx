ce4c02a78138eb22c3989908e6c319d547bc8a8a
Andrew Chan <andrewhongchan@gmail.com>
2024-08-20 11:11:10 -0400

    feat: Selected menu item now applies an active style indicator

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -9,10 +9,10 @@ const Navbar = () => {
     <div className="navbar">
       <img src={assets.logo} alt="" className="logo" />
       <ul className="navbar-menu">
-        <li>home</li>
-        <li>menu</li>
-        <li>mobile-app</li>
-        <li>contact us</li>
+        <li className={menu === "home" ? "active" : ""}>home</li>
+        <li className={menu === "menu" ? "active" : ""}>menu</li>
+        <li className={menu === "mobile-app" ? "active" : ""}>mobile-app</li>
+        <li className={menu === "contact-us" ? "active" : ""}>contact us</li>
       </ul>
       <div className="navbar-right">
         <img src={assets.search_icon} alt="" />
