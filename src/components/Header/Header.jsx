3bd8ab5a2a88e45af8a763e54322cbd1d34f0d1d
Andrew Chan <andrewhongchan@gmail.com>
2024-08-20 11:35:57 -0400

    feat: Header markup for Home page

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,7 +2,13 @@ import "./Header.css"
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className="header">
+        <div className="header-contents">
+            <h2>Order your favourite food here</h2>
+            <p>Choose from a diverse menu featuring a delectable array of dishes crafted with ...</p>
+            <button>ViewMenu</button>
+        </div>
+    </div>
   )
 }
 
