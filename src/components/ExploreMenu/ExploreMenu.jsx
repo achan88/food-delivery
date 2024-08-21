31edbc8849afd60f93a54b112ecc310817f5a9b2
Andrew Chan <andrewhongchan@gmail.com>
2024-08-21 10:20:47 -0400

    feat!: Now outputs menu item list

--- src/components/ExploreMenu/ExploreMenu.jsx
+++ src/components/ExploreMenu/ExploreMenu.jsx
@@ -3,8 +3,19 @@ import { menu_list } from "../../assets/assets"
 
 const ExploreMenu = () => {
   return (
-    <div>
-
+    <div className="explore-menu" id="explore-menu">
+        <h1>Explore our menu</h1>
+        <p className="explore-menu-text">Choose from a dvierse menu feraturing a delectable array of dishes.</p>
+        <div className="explore-menu-list">
+            {menu_list.map((menuItem, index) => {
+                return (
+                    <div key={index} className="explore-menu-list-item">
+                        <img src={menuItem.menu_image} alt="" />
+                        <p>{menuItem.menu_name}</p>
+                    </div>
+                )
+            })}
+        </div>
     </div>
   )
 }
