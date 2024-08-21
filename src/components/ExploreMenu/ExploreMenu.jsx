61463cd8e72f7783d758ed2e044018b38b8fa0f5
Andrew Chan <andrewhongchan@gmail.com>
2024-08-21 10:42:51 -0400

    feat!: Add button handling to show different styling for currently selected menu item

--- src/components/ExploreMenu/ExploreMenu.jsx
+++ src/components/ExploreMenu/ExploreMenu.jsx
@@ -1,23 +1,37 @@
-import "./ExploreMenu.css"
-import { menu_list } from "../../assets/assets"
+import "./ExploreMenu.css";
+import { menu_list } from "../../assets/assets";
 
-const ExploreMenu = ({category}) => {
+const ExploreMenu = ({ category, setCategory }) => {
   return (
     <div className="explore-menu" id="explore-menu">
-        <h1>Explore our menu</h1>
-        <p className="explore-menu-text">Choose from a dvierse menu feraturing a delectable array of dishes.</p>
-        <div className="explore-menu-list">
-            {menu_list.map((menuItem, index) => {
-                return (
-                    <div key={index} className="explore-menu-list-item">
-                        <img src={menuItem.menu_image} alt="" />
-                        <p>{menuItem.menu_name}</p>
-                    </div>
+      <h1>Explore our menu</h1>
+      <p className="explore-menu-text">
+        Choose from a dvierse menu feraturing a delectable array of dishes.
+      </p>
+      <div className="explore-menu-list">
+        {menu_list.map((menuItem, index) => {
+          return (
+            <div
+              onClick={() =>
+                setCategory((prev) =>
+                  prev === menuItem.menu_name ? "All" : menuItem.menu_name
                 )
-            })}
-        </div>
-        <hr />
+              }
+              key={index}
+              className="explore-menu-list-item"
+            >
+              <img
+                className={category === menuItem.menu_name ? "active" : ""}
+                src={menuItem.menu_image}
+                alt=""
+              />
+              <p>{menuItem.menu_name}</p>
+            </div>
+          );
+        })}
+      </div>
+      <hr />
     </div>
-  )
-}
-export default ExploreMenu
+  );
+};
+export default ExploreMenu;
