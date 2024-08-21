bb6cde530d070116198636eb61bf8cb274774261
Andrew Chan <andrewhongchan@gmail.com>
2024-08-21 11:08:36 -0400

    feat: Pass some props down to FoodItem

--- src/components/FoodDisplay/FoodDisplay.jsx
+++ src/components/FoodDisplay/FoodDisplay.jsx
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import "./FoodDisplay.css";
 import { StoreContext } from "../../context/StoreContext";
+import FoodItem from "../FoodItem/FoodItem";
 
 const FoodDisplay = ({ category }) => {
   const { food_list } = useContext(StoreContext);
@@ -9,9 +10,16 @@ const FoodDisplay = ({ category }) => {
       <h2>Dishes near you</h2>
       <div className="food-display-list">
         {food_list.map((foodItem, index) => {
-            return (
-                1 // need some sort of card
-            )
+          return (
+            <FoodItem
+              key={index}
+              id={foodItem._id}
+              name={foodItem.name}
+              description={foodItem.description}
+              price={foodItem.price}
+              image={foodItem.image}
+            />
+          );
         })}
       </div>
     </div>
