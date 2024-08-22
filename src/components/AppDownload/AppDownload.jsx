c323e8f1a249c192c583ab21e94d949f949ece82
Andrew Chan <andrewhongchan@gmail.com>
2024-08-22 12:10:45 -0400

    feat: Add text and Mobile App store icon

--- src/components/AppDownload/AppDownload.jsx
+++ src/components/AppDownload/AppDownload.jsx
@@ -1,8 +1,17 @@
+import { assets } from "../../assets/assets"
 import "./AppDownload.css"
 
 const AppDownload = () => {
   return (
-    <div>AppDownload</div>
+    <div>
+        <div className="app-download" id="app-download">
+            <p>Download our App! <br /> Tomato App</p>
+            <div className="app-download-platforms">
+                <img src={assets.play_store} alt="" />
+                <img src={assets.app_store} alt="" />
+            </div>
+        </div>
+    </div>
   )
 }
 export default AppDownload
