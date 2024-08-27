bb97be86817f14922d89a9f7087721d4fd506048
Andrew Chan <andrewhongchan@gmail.com>
2024-08-27 10:59:32 -0400

    feat: Add cart checkout markup

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -27,13 +27,37 @@ const Cart = () => {
                   <p>${item.price}</p>
                   <p>{cartItems[item._id]}</p>
                   <p>${item.price * cartItems[item._id]}</p>
-                  <p className="cross" onClick={() => removeFromCart(item._id)}>x</p>
+                  <p className="cross" onClick={() => removeFromCart(item._id)}>
+                    x
+                  </p>
                 </div>
                 <hr />
               </>
             );
           }
         })}
+      </div>
+      <div className="cart-bottom">
+        <div className="cart-total">
+          <h2>Cart Totals</h2>
+          <div>
+            <div className="cart-total-details">
+              <p>Subtotal</p>
+              <p>{0}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <p>Delivery Fee</p>
+              <p>{2}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <b>Total</b>
+              <b>{0}</b>
+            </div>
+          </div>
+          <button>PROCEED TO CHECKOUT</button>
+        </div>
       </div>
     </div>
   );
