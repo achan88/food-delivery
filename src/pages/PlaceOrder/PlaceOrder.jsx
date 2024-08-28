1340061e38fb7161ecd2049f92e42e8d9c57a58a
Andrew Chan <andrewhongchan@gmail.com>
2024-08-28 09:46:02 -0400

    feat: Working on the user information form for checkout (added markup)

--- src/pages/PlaceOrder/PlaceOrder.jsx
+++ src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,8 +1,29 @@
-import "./PlaceOrder.css"
+import "./PlaceOrder.css";
 
 const PlaceOrder = () => {
   return (
-    <div>PlaceOrder</div>
-  )
-}
-export default PlaceOrder
+    <form className="place-order">
+      <div className="place-order-left">
+        <p className="title">Delivery Information</p>
+        <div className="multi-fields">
+          <input type="text" placeholder="First Name" />
+          <input type="text" placeholder="Last Name" />
+        </div>
+        <input type="email" placeholder="Email" />
+        <input type="text" placeholder="Street" />
+        <div className="multi-fields">
+          <input type="text" placeholder="City" />
+          <input type="text" placeholder="Province" />
+        </div>
+        <div className="multi-fields">
+          <input type="text" placeholder="Postal Code" />
+          <input type="text" placeholder="Country" />
+        </div>
+        <input type="text" placeholder="Phone Number" />
+      </div>
+      <div className="place-order-right">
+      </div>
+    </form>
+  );
+};
+export default PlaceOrder;
