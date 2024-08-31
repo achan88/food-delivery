af6048675da0df37aa6f35552b7b5a883e9883eb
Andrew Chan <andrewhongchan@gmail.com>
2024-08-31 15:30:28 -0400

    Minor format changes

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <div className="footer" id="footer">
       <div className="footer-content">
         <div className="footer-content-left">
-          <img src={assets.logo} alt="" />
+          <img className="logo" src={assets.logo} alt="" />
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
         <div className="header-contents">
             <h2>Order your favourite food here</h2>
             <p>Choose from a diverse menu featuring a delectable array of dishes crafted with ...</p>
-            <button>ViewMenu</button>
+            <a href="#explore-menu"><button>View Menu</button></a>
         </div>
     </div>
   )
