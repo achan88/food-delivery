962245ac41a65c828289bc6c63f5c30f162496bf
Andrew Chan <andrewhongchan@gmail.com>
2024-08-31 15:33:54 -0400

    Add correct Cart total with delivery fee included

--- src/pages/PlaceOrder/PlaceOrder.jsx
+++ src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,6 +1,12 @@
+import { useContext } from "react";
 import "./PlaceOrder.css";
+import { StoreContext } from "../../context/StoreContext";
 
 const PlaceOrder = () => {
+  const { getTotal } = useContext(StoreContext);
+
+  const cartTotal = getTotal();
+
   return (
     <form className="place-order">
       <div className="place-order-left">
@@ -22,6 +28,26 @@ const PlaceOrder = () => {
         <input type="text" placeholder="Phone Number" />
       </div>
       <div className="place-order-right">
+        <div className="cart-total">
+          <h2>Cart Totals</h2>
+          <div>
+            <div className="cart-total-details">
+              <p>Subtotal</p>
+              <p>${getTotal()}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <p>Delivery Fee</p>
+              <p>${cartTotal > 0 ? 2 : 0}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <b>Total</b>
+              <b>${cartTotal > 0 ? cartTotal + 2 : cartTotal}</b>
+            </div>
+          </div>
+          <button type="button">PROCEED TO PAYMENT</button>
+        </div>
       </div>
     </form>
   );
